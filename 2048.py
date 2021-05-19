3e9576cba55a0c4520bcba49528cd8749101022c
allygeren <allygeren2022@gmail.com>
2021-05-19 14:31:06 -0500

    2048.py

--- 2048.py
+++ 2048.py
@@ -7,7 +7,7 @@ class gamePlan(tk.Frame):
         tk.Frame.__init__(self)
         self.grid()
         self.master.title('2048')
-        self.board = tk.Frame(self,bg= 3, width= 400, height= 400)
+        self.board = tk.Frame(self,bg= c.GRID_COLOR, bd = 3, width= 400, height= 400)
         self.board.grid(pady=(80,0))
         self.create_graphics()
         self.start()
@@ -22,3 +22,23 @@ class gamePlan(tk.Frame):
             temp = []
             for j in range(4):
                 f = tk.Frame(self.board,bg = c.EMPTY_CELL_COLOR, width = 100, height= 100)
+                f.grid(row = i, column = j, padx = 5, pady = 5)
+                num = tk.Label(self.board, bg=c.EMPTY_CELL_COLOR)
+                num.grid(row=i, column=j)
+                data = {"frame": f, "number": num}
+                temp.append(data)
+            self.arr.append(temp)
+        score = tk.Frame(self)
+        score.place(relx=0.5, y=40, anchor="center")
+        tk.Label(score, text = "Score", font = c.SCORE_LABEL_FONT).grid(row = 0)
+        self.score_label = tk.Label(score, text="0", font=c.SCORE_FONT)
+        self.score_label.grid(row=1)
+
+
+
+def main():
+    gamePlan()
+
+
+if __name__ == "__main__":
+    main()
