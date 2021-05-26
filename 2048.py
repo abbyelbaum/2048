53123c8c94ec72705ee75b1a4da64808e505a9fd
allygeren <77809645+allygeren@users.noreply.github.com>
2021-05-25 19:40:46 -0500

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -34,6 +34,8 @@ class gamePlan(tk.Frame):
         tk.Label(score, text = "Score", font = color.SCORE_LABEL_FONT).grid(row = 0)
         self.score_label = tk.Label(score, text="0", font=color.SCORE_FONT)
         self.score_label.grid(row=1)
+        
+    """ Starts game by making grid and filling random 2 cells with 2s """
     def start(self):
         self.matrix = [[0]*4 for _ in range(4)]
         r = random.randint(0,3)
@@ -42,6 +44,8 @@ class gamePlan(tk.Frame):
         self.arr[r][c]['frame'].configure(bg=color.CELL_COLORS[2])
         self.arr[r][c]['number'].configure(bg=color.CELL_COLORS[2], fg=color.CELL_NUMBER_COLORS[2], font=color.CELL_NUMBER_FONTS[2], text='2')
         self.score = 0
+        
+    """ When two neighboring tiles are not the same, this is called to stack them next to each other """
     def stack(self):
         temp = [[0]*4 for _ in range(4)]
         for i in range(4):
@@ -51,6 +55,8 @@ class gamePlan(tk.Frame):
                     temp[i][idx] = self.matrix[i][j]
                     idx += 1
         self.matrix = temp
+    
+    """ When two neighboring tiles are the same, this is called to merge them into one tile with double its value """
     def merge(self):
         for i in range(4):
             for j in range(3):
@@ -58,6 +64,8 @@ class gamePlan(tk.Frame):
                     self.matrix[i][j] *= 2
                     self.matrix[i][j + 1] = 0
                     self.score += self.matrix[i][j]
+    
+    """ Reconfigures board reversed in temporary variable and copies back over into main grid (helper) """
     def backwards(self):
         temp = []
         for i in range(4):
@@ -65,12 +73,16 @@ class gamePlan(tk.Frame):
             for j in range(4):
                 temp[i].append(self.matrix[i][3-j])
         self.matrix = temp
+    
+    """ Reconfigures board by pushing tiles over, uses temporary board and copies into main grid (helper) """
     def translate(self):
         temp = [[0] * 4 for _ in range(4)]
         for i in range(4):
             for j in range(4):
                 temp[i][j] = self.matrix[j][i]
         self.matrix = temp
+    
+    """ Every turn, this is called to randomly add a 2 or 4 to the grid """
     def add(self):
         r = random.randint(0,3)
         c = random.randint(0,3)
@@ -78,6 +90,8 @@ class gamePlan(tk.Frame):
             r = random.randint(0,3)
             c = random.randint(0,3)
         self.matrix[r][c] = random.choice([2,4])
+    
+    """ Updates the GUI """
     def update_graphics(self):
         for i in range(4):
             for j in range(4):
@@ -90,6 +104,8 @@ class gamePlan(tk.Frame):
                     self.arr[i][j]['number'].configure(bg=color.CELL_COLORS[val],fg=color.CELL_NUMBER_COLORS[val],font= color.CELL_NUMBER_FONTS[val], text=str(val))
         self.score_label.configure(text = self.score)
         self.update_idletasks()
+    
+    """ Arrow key methods """
     def left(self,event):
         self.stack()
         self.merge()
@@ -126,6 +142,8 @@ class gamePlan(tk.Frame):
         self.add()
         self.update_graphics()
         self.end()
+        
+    """ The next two methods check if moves are possible (horizontal or vertical) """
     def h_real(self):
         for i in range(4):
             for j in range(3):
@@ -138,6 +156,8 @@ class gamePlan(tk.Frame):
                 if self.matrix[i][j] == self.matrix[i+1][j]:
                     return True
         return False
+    
+    """ Figures out if player won or lost """
     def end(self):
         if any(2048 in row for row in self.matrix):
             end_frame = tk.Frame(self.board,borderwidth=2)
